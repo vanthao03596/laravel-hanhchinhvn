2f4f4a9f215251946d23b89e87a4963ac1c8bbd3
phamthao <phamthao03596@gmail.com>
2021-03-31 23:15:05 +0700

    fix: assertDatabaseCount not found in laravel6,7

--- tests/InstallCommandTest.php
+++ tests/InstallCommandTest.php
@@ -11,9 +11,9 @@ class InstallCommandTest extends TestCase
     {
         $tableNames = config('hanhchinhvn.table_names');
 
-        $this->assertDatabaseCount($tableNames['provinces'], 0);
-        $this->assertDatabaseCount($tableNames['districts'], 0);
-        $this->assertDatabaseCount($tableNames['wards'], 0);
+        $this->assertTrue(DB::table($tableNames['provinces'])->count() === 0);
+        $this->assertTrue(DB::table($tableNames['districts'])->count() === 0);
+        $this->assertTrue(DB::table($tableNames['districts'])->count() === 0);
 
         $this->artisan(Install::class);
 
