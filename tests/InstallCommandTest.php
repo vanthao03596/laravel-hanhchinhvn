cdf94703dfdf7048f4093879657bb6d6f1b09307
phamthao <phamthao03596@gmail.com>
2021-03-31 23:09:03 +0700

    test: fix command test

--- tests/InstallCommandTest.php
+++ tests/InstallCommandTest.php
@@ -2,13 +2,23 @@
 
 namespace Vanthao03596\HCVN\Tests;
 
+use Illuminate\Support\Facades\DB;
 use Vanthao03596\HCVN\Commands\Install;
 
 class InstallCommandTest extends TestCase
 {
     public function test_it_can_install()
     {
-        $this->artisan(Install::class)
-            ->expectsOutput('All done.');
+        $tableNames = config('hanhchinhvn.table_names');
+
+        $this->assertDatabaseCount($tableNames['provinces'], 0);
+        $this->assertDatabaseCount($tableNames['districts'], 0);
+        $this->assertDatabaseCount($tableNames['wards'], 0);
+
+        $this->artisan(Install::class);
+
+        $this->assertTrue(DB::table($tableNames['provinces'])->count() > 0);
+        $this->assertTrue(DB::table($tableNames['districts'])->count() > 0);
+        $this->assertTrue(DB::table($tableNames['districts'])->count() > 0);
     }
 }
