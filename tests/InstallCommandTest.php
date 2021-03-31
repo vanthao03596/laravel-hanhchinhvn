10369e4278a11d7056576fe56cee98e5e036155c
phamthao <phamthao03596@gmail.com>
2021-03-31 15:01:03 +0700

    chore: update deps

--- tests/InstallCommandTest.php
+++ tests/InstallCommandTest.php
@@ -2,10 +2,14 @@
 
 namespace Vanthao03596\HCVN\Tests;
 
+use Vanthao03596\HCVN\Commands\Install;
+
 class InstallCommandTest extends TestCase
 {
     public function test_it_can_install()
     {
-        $this->assertTrue(true);
+        $this->artisan(Install::class)
+            ->expectsOutput('All done.')
+            ->assertExitCode(0);
     }
 }
