9a0a8fe78cdc9130e8693e738b7ab59d029084b3
vanthao03596 <vanthao03596@users.noreply.github.com>
2021-04-01 02:23:10 +0000

    Fix styling

--- tests/InstallCommandTest.php
+++ tests/InstallCommandTest.php
@@ -2,9 +2,6 @@
 
 namespace Vanthao03596\HCVN\Tests;
 
-use Illuminate\Support\Facades\DB;
-use Vanthao03596\HCVN\Commands\Install;
-
 class InstallCommandTest extends TestCase
 {
     public function test_it_can_install()
